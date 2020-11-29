38af51d95908aa7f64d6a6f5b92c17b9d95cd8a4
Wildan Zaim Syaddad <13518068@std.stei.itb.ac.id>
2020-11-29 13:36:26 +0700

    Update App.js

--- willy-wangky-s-factory/src/App.js
+++ willy-wangky-s-factory/src/App.js
@@ -1,24 +1,27 @@
-// import React, { Component } from 'react';
-// import logo from './logo.svg';
-// // import './App.css';
-// import MelihatResep from './components/melihatResep.jsx';
-// import DaftarBahanSupplier from './components/daftarBahanSupplier';
-// import MelihatBahanPabrik from '.components/melihatBahanPabrik.jsx';
-// import MelihatDaftarCoklat from '.components/melihatDaftarCoklat.jsx';
-// import MelihatPemesanan from '.components/melihatPemesanan.jsx';
-// import MelihatSaldo from '.components/melihatSaldo.jsx';
-// //import MembeliBahanSupplier from '.components/membeliBahanSupplier.jsx';
+import React, { Component } from 'react';
+import logo from './logo.svg';
+// import './App.css';
+import MelihatResep from './components/melihatResep.jsx';
+import DaftarBahanSupplier from './components/daftarBahanSupplier';
+import MelihatBahanPabrik from '.components/melihatBahanPabrik.jsx';
+import MelihatDaftarCoklat from '.components/melihatDaftarCoklat.jsx';
+import MelihatPemesanan from '.components/melihatPemesanan.jsx';
+import MelihatSaldo from '.components/melihatSaldo.jsx';
+//import MembeliBahanSupplier from '.components/membeliBahanSupplier.jsx';
 
-// class App extends Component {
-//     render() {
-//       return (
-//         <div className="App">
-//         <MelihatResep /> 
-//         <DaftarBahanSupplier />
-//         <MelihatBahanPabrik />
-//       </div>  
-//       )
-//     }
-//   }
-// export default App;
-// import MelihatBahanPabrik from './components/melihatBahanPabrik';
+class App extends Component {
+    render() {
+      return (
+        <div className="App">
+        <MelihatResep /> 
+        <DaftarBahanSupplier />
+        <MelihatBahanPabrik />
+        <MelihatDaftarCoklat />
+        <MelihatPemesanan />
+        <MelihatSaldo />
+      </div>  
+      )
+    }
+  }
+export default App;
+import MelihatBahanPabrik from './components/melihatBahanPabrik';
