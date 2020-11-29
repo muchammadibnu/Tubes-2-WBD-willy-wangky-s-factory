ea145cc4d0e558e40295d46db8c114692c80cfd7
Muchammad Ibnu <13518072@std.stei.itb.ac.id>
2020-11-29 15:56:44 +0700

    add router and switch

--- willy-wangky-s-factory/src/App.js
+++ willy-wangky-s-factory/src/App.js
@@ -1,27 +1,45 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 // import './App.css';
 import MelihatResep from './components/melihatResep.jsx';
 import DaftarBahanSupplier from './components/daftarBahanSupplier';
-import MelihatBahanPabrik from '.components/melihatBahanPabrik.jsx';
-import MelihatDaftarCoklat from '.components/melihatDaftarCoklat.jsx';
-import MelihatPemesanan from '.components/melihatPemesanan.jsx';
-import MelihatSaldo from '.components/melihatSaldo.jsx';
+import MelihatBahanPabrik from './components/melihatBahanPabrik.jsx';
+import MelihatDaftarCoklat from './components/melihatDaftarCoklat.jsx';
+import MelihatPemesanan from './components/melihatPemesanan.jsx';
+import MelihatSaldo from './components/melihatSaldo.jsx';
 //import MembeliBahanSupplier from '.components/membeliBahanSupplier.jsx';
 
 class App extends Component {
     render() {
       return (
+      <BrowserRouter>
         <div className="App">
-        <MelihatResep /> 
-        <DaftarBahanSupplier />
-        <MelihatBahanPabrik />
-        <MelihatDaftarCoklat />
-        <MelihatPemesanan />
-        <MelihatSaldo />
-      </div>  
+          <Switch>
+            <Route exact path="/">
+              <MelihatResep />
+            </Route>
+            <Route path="/MelihatResep">
+              <MelihatResep />
+            </Route>
+            <Route path="/MelihatBahanSupplier">
+              <DaftarBahanSupplier />
+            </Route>
+            <Route path="/MelihatBahanPabrik">
+              <MelihatBahanPabrik />
+            </Route>
+            <Route path="/MelihatDaftarCoklat">
+              <MelihatDaftarCoklat />
+            </Route>
+            <Route path="/MelihatPemesanan">
+              <MelihatPemesanan />
+            </Route>
+            <Route path="/MelihatSaldo">
+              <MelihatSaldo />
+            </Route>
+          </Switch> 
+        </div>
+      </BrowserRouter>  
       )
     }
   }
 export default App;
-import MelihatBahanPabrik from './components/melihatBahanPabrik';
